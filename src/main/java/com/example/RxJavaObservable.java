24ed5e2cc7463e54e17f64971cc7f81a2743841d
Allan Santos <allancarneirosantos@gmail.com>
2025-12-06 17:49:08 -0500

    feat: other samples

--- src/main/java/com/example/RxJavaObservable.java
+++ src/main/java/com/example/RxJavaObservable.java
@@ -1,7 +1,11 @@
 package com.example;
 
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableSource;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.functions.Supplier;
 import io.reactivex.rxjava3.observables.ConnectableObservable;
 
 import java.util.concurrent.TimeUnit;
@@ -12,6 +16,12 @@ import static com.example.Util.sleep;
 public class RxJavaObservable {
 
     public static void main(String[] args) {
+        example_Using();
+
+        sleep(3_000);
+    }
+
+    private static void example_TwoSubscriptions() {
         ConnectableObservable<Long> connectable
                 = Observable.interval(200, TimeUnit.MILLISECONDS).publish();
 
@@ -26,7 +36,36 @@ public class RxJavaObservable {
         // start processing
         connectable.connect();
 
-        // kill main tread
-        sleep(3_000L);
+    }
+
+    private static void example_Using() {
+
+        // create a resources
+        Supplier<String> resourceSupplier = () -> "TABARNAK";
+
+        // creates an observable that will emit event from the resource
+        Function<String, ObservableSource<? extends Integer>> sourceSupplier = r ->
+                Observable.create(o -> {
+                    for (Character c : r.toCharArray()) {
+                        int ascii = (int) c;
+                        o.onNext(ascii);
+                    }
+                    o.onComplete();
+                });
+
+        Consumer<String> resourceCleanup = r -> System.out.println("Disposed: " + r);
+        Observable<Integer> values = Observable.using(
+                resourceSupplier,
+                sourceSupplier,
+                resourceCleanup
+        );
+
+        StringBuffer buffer = new StringBuffer();
+        // create an Observer / Subscriber to handle the events
+        // each time thet Observable executes no.onNext(c);
+        // this subscribe will receive and handle the message
+        values.subscribe(v -> buffer.append(v).append(".") );
+
+        System.out.println("Result: " + buffer); // 09.121.95.114.101.115.111.117.114.99.101. In ASCII
     }
 }
